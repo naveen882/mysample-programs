3b09651f975d06873904f69375e0ab383214d88a
naveen <naveen882@yahoo.com>
2016-06-20 16:52:41 +0530

    prime number

--- mypg.py
+++ mypg.py
@@ -2719,3 +2719,22 @@ print output
 #/tmp/tutorialsdir/tuts1IbAco8
 
 print "=========================================================="
+#num = int(input("Enter a number: "))
+num = 407
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+print "=========================================================="
