78fe84af2ccb635438cef3c6b4dea37ea6a03a99
naveen <naveen882@yahoo.com>
2017-02-02 20:35:59 +0530

    youtube link fetcher

--- get_all_links_yt.py
+++ get_all_links_yt.py
@@ -75,6 +75,26 @@ href=[]
 #            #        fetch_links(n_link)
 #        except Exception as e:
 #            print e
+import sys
+import random as r
+
+def fetch_side_bar_links(l):
+    http = httplib2.Http()
+    status, response = http.request(l)
+    soup = BeautifulSoup(response)
+    s=soup.findAll("div", { "id" : "watch7-sidebar-modules"})
+    for div in s:
+        new_link = div.find('a')['href']
+        if len(str(new_link)) == '20':
+            new_link = "https://www.youtube.com"+str(new_link)
+            print "=========================="
+            print new_link
+            fetch_links(new_link)
+            break
+
+
+
+f=open('1c_links.txt',"w")
 
 def fetch_links(ln):
     http = httplib2.Http()
@@ -94,11 +114,25 @@ def fetch_links(ln):
                         views = int(views.replace(',','').replace('"',''))
                         if len(href) == 0:
                             l = ln
-                        print ln+ " +++++ " +str(views)
+                            f.write(l+ " +++++ " +str(views))
+                            print l+ " +++++ " +str(views)
+                        else:
+                            if views > 100000000:
+                                f.write(ln+ " +++++ " +str(views))
+                                print ln+ " +++++ " +str(views)
+                            else:
+                                print ln+ " ====> " +str(views)
                         href.append(l)
                         fetch_links(l)
         except Exception as e:
             print e
-            pass
+            if "Only absolute URIs are allowed" in str(e):
+                break
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(exc_type, exc_tb.tb_lineno)
+            fetch_side_bar_links(r.choice(l))
 
-fetch_links('https://www.youtube.com/watch?v=S9bCLPwzSC0')
+#fetch_links('https://www.youtube.com/watch?v=S9bCLPwzSC0')
+fetch_links('https://www.youtube.com/watch?v=rrNbLBS1D2Q') #errortest
+#fetch_side_bar_links('https://www.youtube.com/watch?v=S9bCLPwzSC0')
+f.close()
