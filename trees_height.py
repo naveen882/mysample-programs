4584483d7808af37424ad17c507f6fbc545f467d
naveen <naveen882@yahoo.com>
2017-03-30 14:33:30 +0530

    tree traversal algorithms

--- trees_height.py
+++ trees_height.py
@@ -1,61 +1,48 @@
 class trees(object):
-	def __init__(self,name,left=None,right=None):
-		self.name = name
-		self.left = None
-		self.right = None
+    def __init__(self,name,left=None,right=None):
+        self.name = name
+        self.left = None
+        self.right = None
 
-	@staticmethod
-	def display(node,mode):
-		count = 0
-		while node:
-			print node.name
-			count += 1
-			print "Depth is {count}".format(**locals())
-			node_tmp = node
-			if mode == 'left':
-				if node.left != None and node.right != None:
-					node = node_tmp
-				elif node.left != None:
-					node = node.left
-				elif node.right != None:
-					node = node.right
-				else:
-					break
-			elif mode == 'right':
-				visited_right = 0
-				if node.right != None:
-					node_tmp = node
-					node = node.left
-					if node.right == None:
-						node = node_tmp
-					else:
-						continue
-					visited_right = 1
-					node = node.right
-				elif node.left != None:
-					visited_right = 2
-					node = node.left
-				else:
-					break
-				
-			
-def inorderTraversal(root):
-	return (inorderTraversal(root.left) + [root.name] + inorderTraversal(root.right)) if root else []
-a=0
-def inorderTraversal(root):
-	global a
-	res = []
-	if root:
-		a+=1
-		res = inorderTraversal(root.left) 
-		print root.name
-		print a
-		res.append(root.name)
-		#res = res + inorderTraversal(root.right)
-	return res
+    @staticmethod
+    def inorderTraversal(root):
+        if root:
+            print "====1"
+            print root.name
+            print "====2"
+            trees.inorderTraversal(root.left) 
+            print "====3"
+            print root.name
+            print "====4"
+            trees.inorderTraversal(root.right)
+            print "====5"
 
+    @staticmethod
+    def preorder(root):
+        if root:
+            print root.name
+            trees.preorder(root.left)
+            trees.preorder(root.right)
 
+    @staticmethod
+    def postorder(root):
+        if root:
+            trees.postorder(root.left)
+            trees.postorder(root.right)
+            print root.name
 
+#def inorderTraversal(root):
+#	return (inorderTraversal(root.left) + [root.name] + inorderTraversal(root.right)) if root else []
+
+#def inorderTraversal(root):
+#	global a
+#	res = []
+#	if root:
+#            res = inorderTraversal(root.left) 
+#            print root.name
+#            inorderTraversal(root.right)
+#            #res = res + inorderTraversal(root.right)
+#	return res
 
 t1=trees('A')
 t2=trees('B')
@@ -70,16 +57,18 @@ t10=trees('J')
 
 t1.left = t2
 t1.right = t3
-t2.left= t4
-t3.right = t5
-t4.right = t6
-t4.left = t10
-t6.right = t7
-t7.right = t8
-t8.right = t9
+#t2.left= t4
+#t2.right = t5
+#t4.right = t6
+#t4.left = t10
+#t6.right = t7
+#t7.right = t8
+#t8.right = t9
 
-#trees.display(t1,'left')
-print inorderTraversal(t1)
+print "========Inordertraversal============"
+trees.inorderTraversal(t1)
+print "========Preordertraversal============"
+trees.preorder(t1)
+print "========Postordertraversal============"
+trees.postorder(t1)
 print "====================================="
-#trees.display(t1,'right')
-		
