c0303435c971aa27a898a8a37c5c0e76045d8677
naveen882 <navin882@gmail.com>
2017-07-25 13:02:15 +0530

    Update mypg.py

--- mypg.py
+++ mypg.py
@@ -3886,3 +3886,9 @@ In python 3
 """
 
 print "=========================================================="
+"""
+Serialization is the process of converting an object into a stream of bytes in order to store the object 
+or 
+transmit it to memory, a database, or a file. Its main purpose is to save the state of an object in 
+order to be able to recreate it when needed. The reverse process is called deserialization.
+"""
