793fcbf207729534319bb532c05d960fa79c5264
naveen882 <navin882@gmail.com>
2017-08-25 21:00:45 +0530

    Update last_day_quarter.py

--- last_day_quarter.py
+++ last_day_quarter.py
@@ -2,14 +2,14 @@ import datetime as dt
 import calendar
 
 month_quarter = [3,6,9,12]
-d= dt.datetime.strptime('2017-12-11', "%Y-%m-%d").date()
-if d.month in month_quarter:
-    last_day = calendar.monthrange(d.year,d.month)[1]
-    actual_date =  str(d.year)+"-"+str(d.month) +"-"+str(last_day)
-    d1= dt.datetime.strptime(actual_date, "%Y-%m-%d").date()
-    if d == d1:
+user_date = dt.datetime.strptime('2017-12-31', "%Y-%m-%d").date()
+if user_date.month in month_quarter:
+    last_day = calendar.monthrange(user_date.year,user_date.month)[1]
+    actual_date =  dt.date(user_date.year,user_date.month, last_day)
+    if user_date == actual_date:
         print "Matching"
     else:
         print "Select last day of the Quarter"
 else:
     print "Select last day of the Quarter"
+
