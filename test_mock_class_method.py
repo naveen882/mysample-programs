87bd3ef41f56050a1408d96045f699b54444ed78
naveen882 <navin882@gmail.com>
2018-06-29 21:03:12 +0530

    test_mock_class_method.py

---
+++ test_mock_class_method.py
@@ -0,0 +1,24 @@
+class A(object):
+    def __init__(self):
+        self.a =True
+        
+    def rt(self):
+        return self.a
+    
+    def show(self):
+        a = self.rt()                  
+        return a
+
+a=A()
+print "===="
+a.show()
+
+
+class test_A(TestCase):
+
+    def test_A(self):
+        print "In A======"
+        a=A()
+        self.assertFalse(a.show())
+        
+unittest.main()
